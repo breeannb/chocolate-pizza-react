f26c0b1fde78e893c99a764fc2c8010181efaff1
Annalise Murphy <nali247@gmail.com>
2020-04-27 17:51:49 -0700

    Facebook Icon added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header1 from './Header1';
 import './App.css';
 import Logo from './Logo'; 
 import Header2 from './Header2'; 
+import IconFacebook from './IconFacebook';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
      <Logo /> 
      <Header1 />
      <Header2 />
+     <IconFacebook />
                 {/* Social Media Icon 1 prop link facebook https.//www.facebook.com*/}
                 {/* Social Media Icon 2 prop link twitter https.//www.twitter.com*/}
                 {/* Social Media Icon 3 prop link facebook https://aboutme.google.com*/}
---
+++ src/IconFacebook.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+// import fbicon from './fb-icon.png';
+
+class IconFacebook extends Component {
+    render() {
+        return (
+            // <div className="icon-facebook">
+            //     <img alt='' src={fbicon} />
+            // </div>
+            <div>
+                This is the website {this.props.websitelink}
+            </div>
+        );
+    }
+}
+export default class PutEmAllTogether extends React.Component {
+    render() {
+        return (
+            <IconFacebook websitelink="http://www.facebook.com" />
+
+        );
+    }
+}
