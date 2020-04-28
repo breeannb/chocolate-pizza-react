44f8105f206864901c91d1c31a41a42f6224fd55
Annalise Murphy <nali247@gmail.com>
2020-04-27 17:19:04 -0700

    Delicious added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header1 from './Header1';
 import './App.css';
 import Logo from './Logo'; 
 
@@ -7,6 +7,11 @@ function App() {
   return (
     <div>
      <Logo /> 
+     <Header1 />
+                {/* H2: The Best Food Blog on the Web */}
+                {/* Social Media Icon 1 prop link facebook https.//www.facebook.com*/}
+                {/* Social Media Icon 2 prop link twitter https.//www.twitter.com*/}
+                {/* Social Media Icon 3 prop link facebook https://aboutme.google.com*/}
     </div>
   );
 }
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import Logo from './Logo.js';
+// import React, { Component } from 'react';
+// import Logo from './Logo.js';
 
 
 
-export default class NavBar extends Component {
-    render() {
-        return (
-            <div className='nav-bar'>
-                <Logo />
-                {/* Title: Delicious */} 
-                {/* H2: The Best Food Blog on the Web */}
-                {/* Social Media Icon 1 prop link facebook https.//www.facebook.com*/}
-                {/* Social Media Icon 2 prop link twitter https.//www.twitter.com*/}
-                {/* Social Media Icon 3 prop link facebook https://aboutme.google.com*/}
+
+// export default class NavBar extends Component {
+//     render() {
+//         return (
+//             <div className='nav-bar'>
+//                 <Logo />
+                
 
 
-            </div>
-        )
-    }
-}
+//             </div>
+//         )
+//     }
+// }
