1212480081476568d6ed136a9ef313f49e654209
Breeann Bolinsky <bolinskybm10@gmail.com>
2020-04-28 08:46:27 -0700

    started stretch goal but cant finish before class will work on it later

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,17 +1,31 @@
 // import React, { Component } from 'react';
-// import Logo from './Logo.js';
+// import links from './data.js';
 
 
-
+// class ListOfLinks extends Component {
+//     render() {
+//         return ( 
+//             <div className='nav-link'>
+//                 <a href={this.props.location}>
+//                 {this.props.description}
+//                 </a>
+//             </div>
+//         )
+//     }
+// }
 
 // export default class NavBar extends Component {
 //     render() {
 //         return (
 //             <div className='nav-bar'>
-//                 <Logo />
-                
-
-
+//          {
+//              this.props.map((linkItem) => {
+//                  return <ListOfLinks 
+//                  location={ linkItem.location}
+//                  description={ linkItem.description}
+//                  />
+//              })
+//          }       
 //             </div>
 //         )
 //     }
---
+++ src/data.js
@@ -0,0 +1,29 @@
+const links = [
+    {
+        location: 'https://www.instagram.com',
+        description: 'instagram'
+
+    },
+    {
+        location: 'https://www.flickr.com/',
+        description: 'flicker'
+
+    },
+    {
+        location: 'https://www.pinterest.com/',
+        description: 'pinterest'
+
+    },
+    {
+        location: 'https://en.wikipedia.org/wiki/RSS',
+        description: 'rss'
+
+    },
+    {
+        location: 'https://mail.google.com',
+        description: 'email'
+
+    }
+];
+
+export default links; 
